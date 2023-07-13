a5061f83140da6257315b473af8d9b30349b9919
Swastik <96874651+stevebnm@users.noreply.github.com>
2023-07-13 20:37:11 +0530

    Update run.py

--- run.py
+++ run.py
@@ -5,12 +5,75 @@ from app import create_app
 
 from dotenv import load_dotenv
 
-dotenv_path = join(dirname(__file__), '.env')  # Address of your .env file
-load_dotenv(dotenv_path)
+# Imports
+from flask import (Flask,
+                   redirect,
+                   url_for,
+                   request,
+                   render_template)
+import mysql.connector
+import json
 
+app = Flask(__name__,template_folder="templates")
 
-config_name = os.getenv('FLASK_CONFIG')
-app = create_app(config_name)
+# Connecting to Sql
+config = {
+    'user': 'root',
+    'password':  'root',
+    'host': 'db',
+    'port': '3306',
+    'database': 'Employee'
+}
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+connection = mysql.connector.connect(**config)
+cursor = connection.cursor()
+
+# Home Method
+@app.route("/", methods=['GET','POST'])
+def home():
+
+    # Post request
+    if request.method == "POST":
+
+        # Get Data from Form
+        name = request.form['name']
+        job = request.form['job']
+        salary = request.form['salary']
+
+        # Insert into employee Table
+        addEmployee = ("INSERT INTO employee "
+              "(name, job, salary) "
+              "VALUES (%(name)s, %(job)s, %(salary)s)")
+        
+        dataEmployee = {
+            'name': name,
+            'job': job,
+            'salary': salary,
+        }
+        cursor.execute(addEmployee,dataEmployee)
+        connection.commit()
+
+    # Get All Employees
+    getEmployees = "select * from employee"
+    cursor.execute(getEmployees)
+    AllEmployees = cursor.fetchall()
+
+    # Render Home Page
+    return render_template('home.html',allEmployees=AllEmployees)
+
+# Delete Method
+@app.route("/delete", methods=['POST'])
+def delete():
+
+    # Get ID from form
+    id_ = (request.form['id'],)
+    # Delete From employee Table
+    deleteEmployee = """ Delete from employee where id = %s """
+    cursor.execute(deleteEmployee,id_)
+    connection.commit()
+
+    # Redirect to Home page
+    return redirect(url_for('home'))
+
+if __name__ == '__main__':
+    app.run(debug = True,host='0.0.0.0')
